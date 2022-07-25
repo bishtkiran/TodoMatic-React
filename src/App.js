3be8baa5b38891ac54e75fc7d3a2de55f483bbd4
Kiran Bisht <kiranbisht338@gmail.com>
2022-07-25 12:01:43 +0530

    added third component - filterbutton component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 function App(props) {
   const tasksList = props.tasks.map((task) => 
   <Todo name={task.name} completed={task.completed} id={task.id} key={task.id}/>)
@@ -8,22 +9,10 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-      </div>
+      <FilterButton text="All" />
+      <FilterButton text="Active" />
+      <FilterButton text="Completed"/>
+    </div>
       <h2 id="list-heading">
         3 tasks remaining
       </h2>
