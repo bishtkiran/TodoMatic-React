559786798beaf67c4504f7cd6e596d99756a6806
Kiran Bisht <kiranbisht338@gmail.com>
2022-07-25 12:24:26 +0530

    added dummy call-back prop to Form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 function App(props) {
+  function addTask(name){
+    alert(name);
+  }
   const tasksList = props.tasks.map((task) => 
   <Todo name={task.name} completed={task.completed} id={task.id} key={task.id}/>)
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
       <FilterButton text="All" />
       <FilterButton text="Active" />
