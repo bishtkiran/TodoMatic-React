80a1db505b4ce1b13fe3cf0d61215ec3a0067b4e
Kiran Bisht <kiranbisht338@gmail.com>
2022-07-25 14:08:19 +0530

    added functionality to filter tasks as active, completed and all

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
@@ -32,10 +43,19 @@ function App(props) {
   }
   
 
-  const tasksList = tasks.map((task) =>
+  const tasksList = tasks.filter(FILTER_MAP[filter]).map((task) =>
     <Todo name={task.name} completed={task.completed} id={task.id}
       key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} 
       editTask={editTask} />)
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter} />
+  ));      
+      
   const tasksNoun = tasksList.length > 1 ? 'tasks' : 'task';
   const headingText = `${tasksList.length} ${tasksNoun} remaining`;
   return (
@@ -43,9 +63,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton text="All" />
-        <FilterButton text="Active" />
-        <FilterButton text="Completed" />
+      {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
